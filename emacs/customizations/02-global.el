8a71a818b55dba5c5674ec221edcb0476fc5fafb
Daniel Fuentes <Daniel.Fuentes@clever.com>
2014-09-15 17:51:18 -0700

    fix clipboard issue in mac os x

--- emacs/customizations/02-global.el
+++ emacs/customizations/02-global.el
@@ -1,5 +1,11 @@
 (require 'dired-details+)
 (require 'color-theme)
 (load-theme 'tango-dark)
+(when (eq window-system 'ns) 
+  (defadvice ns-get-pasteboard (around hack-empty-pasteboard compile activate) 
+    (condition-case err 
+        ad-do-it 
+      (quit (message "%s" (cadr err)) 
+            nil)))) 
 (provide  '02-global)
 
